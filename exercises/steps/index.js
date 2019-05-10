a99c532605e75580253fc2b49e1e6888734704e8
dev-iskan <successfuliskan@gmail.com>
2019-05-10 21:41:17 +0500

    steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+// solution 1
+// simple matrix where if column less or equal to row we print # else whitespace
+// function steps(n) {
+//   for(let row = 0; row<n; row++) {
+//     let stair = ''
+
+//     for(let column = 0; column<n; column++) {
+//       if(column<=row) { stair += '#' }
+//       else { stair += ' ' }
+//     }
+
+//     debugger;
+    
+//     console.log(stair)
+//   }
+// }
+
+
+
+// solution 2
+// recursion solution
+// n is number, row is row and stair is the string values of row
+function steps(n, row = 0, stair = '') {
+  // stop case, when n becomes equal to row
+  if(n === row) {
+    return
+  }
+
+  // if n is equal to stair length, we level up to next row and console log whole row
+  if(n === stair.length) {
+    console.log(stair)
+    steps(n, row + 1)
+    return 
+  }
+
+  // else if add to stair either # or whitespace
+  stair += stair.length <= row ? '#' : ' ' 
+
+  // recurse to next column
+  steps(n, row, stair)
+}
 
 module.exports = steps;
