31a6615884201031b7309f290bedd04154be774d
dev-iskan <successfuliskan@gmail.com>
2019-05-15 01:18:13 +0500

    vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+
+// solution 1
+// function vowels(str) {
+//   let counter = 0
+//   const checker = ['a', 'e', 'i', 'o', 'u']
+//   for(let char of str.toLowerCase()) {
+//     if(checker.includes(char)) counter++
+//   }
+
+//   return counter
+// }
+
+
+// solution 2
+function vowels(str) {
+  // flag g - continue checking, i - case insensitive
+
+  // if no matches null, else array
+  const matches = str.match(/[aeuio]/gi)
+
+
+  // returns length if matches not null else 0
+  return matches && matches.length || 0
+}
 
 module.exports = vowels;
