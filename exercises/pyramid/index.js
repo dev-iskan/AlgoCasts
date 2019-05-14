132b0ab0da2ede98abbbe23a96e7f08ba4a4355c
dev-iskan <successfuliskan@gmail.com>
2019-05-15 01:01:30 +0500

    pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,58 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
 
+// solution 1
+function pyramid(n) {
+  // midpoint is found by logic, when n rows columns are (2 * n - 1)
+  const midpoint = Math.floor((2*n-1) / 2)
+
+  // ittearte through rows
+  for(let row = 0; row < n; row++) {
+    // assign level
+    let level = ''
+
+    // itterate thorugh columns lemgth
+    for(let column = 0; column < 2 * n -1; column++) {
+      // if the calc less equal and more equal to column then # otherwise space
+      if(midpoint - row <= column && midpoint + row >= column) {
+        level +='#'
+      } else {
+        level +=' '
+      }
+    }
+
+    console.log(level)
+  }
+}
+
+
+
+
+// solution 2
+function pyramid(n, row = 0, level = '') {
+  // stop case
+  if(row === n) {
+    return
+  }
+
+  // case when row exceeded
+  if(level.length === (2*n)-1) {
+    console.log(level)
+    pyramid(n, row + 1)
+    return
+  }
+
+
+  const midpoint = Math.floor((2 * n -1) /2)
+
+  let add = midpoint - row <=level.length && midpoint + row >= level.length ? '#' : ' '
+
+  // next itteration
+  pyramid(n, row, level + add)
+}
+
+
+
+pyramid(50)
 module.exports = pyramid;
