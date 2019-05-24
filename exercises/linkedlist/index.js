1798b6e6d02cfb974bbc00a2b97f5e8f767b93f8
dev-iskan <successfuliskan@gmail.com>
2019-05-25 01:14:23 +0500

    insertFirst method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -9,6 +9,18 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor (head = null) {
+    this.head = null
+  }
+
+  insertFirst (data) {
+    // assign next to head either null || old node
+    const newNode = new Node(data, this.head)
+
+    // assign new head to new node itself
+    this.head = newNode
+  }
+}
 
 module.exports = { Node, LinkedList };
