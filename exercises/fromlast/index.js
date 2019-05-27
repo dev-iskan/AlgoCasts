cba4d190179c889b468c6d8efe267648c0f8bf8a
dev-iskan <successfuliskan@gmail.com>
2019-05-27 20:59:57 +0500

    from last problem

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,26 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    // initialize two pointing nodes
+    let slow = list.getFirst()
+    let fast = list.getFirst()
+
+    // set fast to position from slow to n
+    while (n > 0) {
+      fast = fast.next
+      n--
+    }
+
+    // iterate untill end of list
+    while (fast.next) {
+      slow = slow.next
+      fast = fast.next
+    }
+
+    // return slow node
+
+    return slow
+}
 
 module.exports = fromLast;
