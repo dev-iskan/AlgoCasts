123654d1428c0bdf9d7d17fd08e6496c1e164bf4
dev-iskan <successfuliskan@gmail.com>
2019-05-28 14:50:49 +0500

    levelwidth problem

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  // init array with root and stop character
+  const arr = [root, 's']
+  // init widths array
+  const widths = [0]
+
+  // iterate untill array consists only stop character
+  while (arr.length > 1) {
+    const node = arr.shift()
+
+    // if poped node is stop character push new counter to widths and push stop char again to arr
+    if (node === 's') {
+      widths.push(0)
+      arr.push('s')
+    } else {
+      // else push children and increment counter on the level
+      arr.push(...node.children)
+      widths[widths.length - 1]++
+    }
+  }
+
+  return widths
+}
 
 module.exports = levelWidth;
